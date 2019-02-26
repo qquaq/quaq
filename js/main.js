89ada29ee703d03c19c9e86565e1effe727ee6c3
quaq <rotmgsimonu@gmail.com>
2019-02-25 19:01:46 -0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,8 +11,7 @@ function openTab(evt, tabTitle) {
     document.getElementById(tabTitle).style.display = "block";
     evt.currentTarget.className += " active";
 };
-var slideIndex = [1,1,1,1,1,1,1,1,1,1];
-/* Class the members of each slideshow group with different CSS classes */
+var slideIndex = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 
 var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8", "mySlides9", "mySlides10"];
 showSlides(1, 0);
@@ -28,119 +27,123 @@ showSlides(1, 9);
 
 
 function plusSlides(n, no) {
-    $('.vid').each(function() {
+    $('.vid').each(function () {
         $(this).get(0).pause();
     });
-  showSlides(slideIndex[no] += n, no);
+    showSlides(slideIndex[no] += n, no);
 };
 
 function showSlides(n, no) {
-  var i;
-  var x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1} 
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none"; 
-  }
-  x[slideIndex[no]-1].style.display = "block"; 
+    var i;
+    var x = document.getElementsByClassName(slideId[no]);
+    if (n > x.length) {
+        slideIndex[no] = 1
+    }
+    if (n < 1) {
+        slideIndex[no] = x.length
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex[no] - 1].style.display = "block";
 }
 
 var ft = document.querySelectorAll('.fromtop');
 
 var nodeList = anime({
-  targets: ft,
-  translateY: [-500,0],
-  duration: 4000,
-  elasticity: 10
+    targets: ft,
+    translateY: [-500, 0],
+    duration: 4000,
+    elasticity: 10
 });
 
 var fl = document.querySelectorAll('.fromleft');
 
 var duckAnim = anime({
-  targets: fl,
-  translateX: [-1000,0],
-  duration: 3000,
-  elasticity: 15,
-  easing: 'easeInOutQuart'
+    targets: fl,
+    translateX: [-1000, 0],
+    duration: 3000,
+    elasticity: 15,
+    easing: 'easeInOutQuart'
 });
 
 var fl2 = document.querySelectorAll('.fromleft2');
 
 var textAnim1 = anime({
-  targets: fl2,
-  translateX: [-1500,0],
-  duration: 1000,
-  elasticity: 5,
-  easing: 'easeInOutQuart',
-  autoplay: false,
+    targets: fl2,
+    translateX: [-1500, 0],
+    duration: 1000,
+    elasticity: 5,
+    easing: 'easeInOutQuart',
+    autoplay: false,
 });
 
 var fl3 = document.querySelectorAll('.fromleft3');
 
 var textAnim2 = anime({
-  targets: fl3,
-  translateX: [-1502,0],
-  duration: 2000,
-  elasticity: 5,
-  easing: 'easeInOutQuart',
-  autoplay: false,
+    targets: fl3,
+    translateX: [-1502, 0],
+    duration: 2000,
+    elasticity: 5,
+    easing: 'easeInOutQuart',
+    autoplay: false,
 });
 
 
 var seekProgressEl = document.querySelector('#seekAnim .progress');
 
-if (seekProgressEl){
-    seekProgressEl.addEventListener('input', function() { 
-      duckAnim.seek(duckAnim.duration * (document.body.scrollTop / 10));
+if (seekProgressEl) {
+    seekProgressEl.addEventListener('input', function () {
+        duckAnim.seek(duckAnim.duration * (document.body.scrollTop / 10));
     });
 }
 
 var fb1 = document.querySelectorAll('.frombottom1');
 
 var nodeList = anime({
-  targets: fb1,
-  translateY: [500,0],
-  duration: 1500,
-  elasticity: 15
+    targets: fb1,
+    translateY: [500, 0],
+    duration: 1500,
+    elasticity: 15
 });
 
 var fb2 = document.querySelectorAll('.frombottom2');
 
 var nodeList = anime({
-  targets: fb2,
-  translateY: [500,0],
-  duration: 2500,
-  elasticity: 15
+    targets: fb2,
+    translateY: [500, 0],
+    duration: 2500,
+    elasticity: 15
 });
 
 var fb3 = document.querySelectorAll('.frombottom3');
 
 var nodeList = anime({
-  targets: fb3,
-  translateY: [500,0],
-  duration: 3500,
-  elasticity: 15
+    targets: fb3,
+    translateY: [500, 0],
+    duration: 3500,
+    elasticity: 15
 });
 
 var fb4 = document.querySelectorAll('.frombottom4');
 
 var nodeList4 = anime({
-  targets: fb4,
-  translateY: [1000,0],
-  duration: 1000,
-  elasticity: 20,
-  autoplay: false
+    targets: fb4,
+    translateY: [1000, 0],
+    duration: 1000,
+    elasticity: 20,
+    autoplay: false
 });
 
 
-$('.in1').waypoint(function() {
+$('.in1').waypoint(function () {
     textAnim1.play();
     textAnim2.play();
 }, {
     offset: '100%'
 });
 
-$('.in2').waypoint(function() {
+$('.in2').waypoint(function () {
     nodeList4.play();
 }, {
     offset: 50
